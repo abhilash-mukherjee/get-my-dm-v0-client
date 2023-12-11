2b6d393eb04734fb548c22a3c2c59bedfce36c74
Abhilash <abhilash301267@gmail.com>
2023-12-11 16:58:43 +0530

    removed unnecessary imports from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import axios from 'axios'
 import './App.css'
 import {BASE_URL} from './helper'
