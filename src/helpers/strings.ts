365459db03b2782a788e84dd3cb7daca721315d6
Abhilash <abhilash301267@gmail.com>
2023-12-19 18:10:24 +0530

    Shifted backend to vercel

--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,4 +1,4 @@
-export const BASE_URL =  'https://rose-frantic-jellyfish.cyclic.app';
+export const BASE_URL =  'https://get-my-dm-v0-server.vercel.app';
 // export const BASE_URL =  'http://localhost:3000';
 export const TOKEN = 'token';
 export const PROMOTION_URL = 'getmydm.com/'
