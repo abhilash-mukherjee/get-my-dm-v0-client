848a1ba89dd4fc04867ef94b8de8cda256c5345c
Abhilash <abhilash301267@gmail.com>
2023-12-23 21:21:52 +0530

    Changed string since now I am pusing to prod

--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL =  'https://get-my-dm-v0-server.vercel.app';
-export const BASE_URL =  'http://localhost:3000';
+export const BASE_URL =  'https://get-my-dm-v0-server.vercel.app';
+// export const BASE_URL =  'http://localhost:3000';
 export const TOKEN = 'token';
 export const PROMOTION_URL = 'getmydm.com/'
 export const DELIVERED='delivered';
